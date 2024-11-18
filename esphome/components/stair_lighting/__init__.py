36000e1ea42c4d529897a5db58ea06f162f5dfc4
Valeriy Ievskiy <v.ievskii@bftcom.com>
2024-11-19 00:28:05 +0300

    add automation

--- esphome/components/stair_lighting/__init__.py
+++ esphome/components/stair_lighting/__init__.py
@@ -1,11 +1,19 @@
+from esphome import automation
 import esphome.codegen as cg
 from esphome.components import light
 import esphome.config_validation as cv
 from esphome.const import CONF_ADDRESSABLE_LIGHT_ID, CONF_ID
 
+AUTO_LOAD = ["sensor", "binary_sensor", "switch"]
+
+# Config constants
+CONF_STEPS = "steps"
 CONF_START = "start"
 CONF_END = "end"
+CONF_EFFECT_BRIGHTNESS = "effect_brightness"
+CONF_NIGHT_BRIGHTNESS = "night_brightness"
 
+# Classes
 stair_lighting_ns = cg.esphome_ns.namespace("stair_lighting")
 
 StairLightingComponent = stair_lighting_ns.class_(
@@ -13,7 +21,12 @@ StairLightingComponent = stair_lighting_ns.class_(
 )
 StairLightingStep = stair_lighting_ns.class_("StairLightingStep")
 StairLightingEffect = stair_lighting_ns.class_("StairlightingEffect")
-
+StairLightingTurnOnAction = stair_lighting_ns.class_(
+    "StairLightingTurnOnAction", automation.Action
+)
+StairLightingTurnOffAction = stair_lighting_ns.class_(
+    "StairLightingTurnOffAction", automation.Action
+)
 
 STEP_SCHEMA = cv.Schema(
     {
@@ -23,15 +36,6 @@ STEP_SCHEMA = cv.Schema(
         cv.Required(CONF_END): cv.positive_int,
     }
 )
-
-CONFIG_SCHEMA = cv.Schema(
-    {
-        cv.GenerateID(): cv.declare_id(StairLightingComponent),
-        cv.Required("steps"): cv.All(cv.ensure_list(STEP_SCHEMA), cv.Length(min=1)),
-        cv.Optional("night_brightness", default="10%"): cv.percentage,
-        cv.Optional("effect_brightness", default="100%"): cv.percentage,
-    }
-).extend(cv.COMPONENT_SCHEMA)
 
 
 async def step_to_code(config):
@@ -44,13 +48,61 @@ async def step_to_code(config):
     return var
 
 
+CONFIG_SCHEMA = cv.Schema(
+    {
+        cv.GenerateID(): cv.declare_id(StairLightingComponent),
+        cv.Required(CONF_STEPS): cv.All(cv.ensure_list(STEP_SCHEMA), cv.Length(min=1)),
+        cv.Optional(CONF_EFFECT_BRIGHTNESS, default="100%"): cv.percentage,
+        cv.Optional(CONF_NIGHT_BRIGHTNESS, default="10%"): cv.percentage,
+    }
+).extend(cv.COMPONENT_SCHEMA)
+
+
 async def to_code(config):
     var = cg.new_Pvariable(config[CONF_ID])
     steps = []
-    for step_config in config.get("steps", []):
+    for step_config in config.get(CONF_STEPS, []):
         steps.append(await step_to_code(step_config))
     cg.add(var.add_steps(steps))
-    cg.add(var.set_night_brightness(config["night_brightness"]))
-    cg.add(var.set_effect_brightness(config["effect_brightness"]))
+    cg.add(var.set_effect_brightness(config[CONF_EFFECT_BRIGHTNESS]))
+    cg.add(var.set_night_brightness(config[CONF_NIGHT_BRIGHTNESS]))
 
     await cg.register_component(var, config)
+
+
+@automation.register_action(
+    "stair_lighting.turn_on",
+    StairLightingTurnOnAction,
+    cv.Schema(
+        {
+            cv.Required(CONF_ID): cv.use_id(StairLightingComponent),
+            cv.Optional(CONF_EFFECT_BRIGHTNESS): cv.templatable(cv.percentage),
+            cv.Optional(CONF_NIGHT_BRIGHTNESS): cv.templatable(cv.percentage),
+        }
+    ),
+)
+async def stair_lighting_turn_on_to_code(config, action_id, template_arg, args):
+    paren = await cg.get_variable(config[CONF_ID])
+    var = cg.new_Pvariable(action_id, template_arg, paren)
+    if CONF_EFFECT_BRIGHTNESS in config:
+        templ = await cg.templatable(config[CONF_EFFECT_BRIGHTNESS], args, float)
+        cg.add(var.set_effect_brightness(templ))
+    if CONF_NIGHT_BRIGHTNESS in config:
+        templ = await cg.templatable(config[CONF_NIGHT_BRIGHTNESS], args, float)
+        cg.add(var.set_night_brightness(templ))
+    return var
+
+
+@automation.register_action(
+    "stair_lighting.turn_off",
+    StairLightingTurnOffAction,
+    cv.Schema(
+        {
+            cv.Required(CONF_ID): cv.use_id(StairLightingComponent),
+        }
+    ),
+)
+async def stair_lighting_turn_off_to_code(config, action_id, template_arg, args):
+    paren = await cg.get_variable(config[CONF_ID])
+    var = cg.new_Pvariable(action_id, template_arg, paren)
+    return var
--- esphome/components/stair_lighting/stair_lighting.h
+++ esphome/components/stair_lighting/stair_lighting.h
@@ -1,6 +1,7 @@
 #pragma once
 
 #include "esphome/core/component.h"
+#include "esphome/core/automation.h"
 #include "esphome/core/color.h"
 // #include "esphome/components/sensor/sensor.h"
 #include "stair_lighting_step.h"
@@ -19,8 +20,8 @@ class StairLightingComponent : public PollingComponent {
   // void set_lower_sensor(sensor::Sensor *lower_sensor) { lower_sensor_ = lower_sensor; }
   // void set_upper_sensor(sensor::Sensor *upper_sensor) { upper_sensor_ = upper_sensor; }
   void set_effect(StairLightingEffect *effect) { effect_ = effect; }
-  void set_night_brightness(float night_brightness) { night_brightness_ = night_brightness; }
   void set_effect_brightness(float effect_brightness) { effect_brightness_ = effect_brightness; }
+  void set_night_brightness(float night_brightness) { night_brightness_ = night_brightness; }
 
   void setup() override;
   void update() override;
@@ -31,8 +32,43 @@ class StairLightingComponent : public PollingComponent {
   // sensor::Sensor *lower_sensor_ = nullptr;
   StairLightingEffect *effect_ = nullptr;
 
-  float night_brightness_ = 0;
   float effect_brightness_ = 0;
+  float night_brightness_ = 0;
+};
+
+template<typename... Ts> class StairLightingTurnOnAction : public Action<Ts...> {
+ public:
+  StairLightingTurnOnAction(StairLightingComponent *stair_lighting) : stair_lighting_(stair_lighting) {}
+
+  TEMPLATABLE_VALUE(float, effect_brightness)
+  TEMPLATABLE_VALUE(float, night_brightness)
+
+  void play(Ts... x) override {
+    optional<float> effect_brightness = this->effect_brightness_.optional_value(x...);
+    if (effect_brightness.has_value()) {
+      stair_lighting_->set_effect_brightness(*effect_brightness);
+    }
+    optional<float> night_brightness = this->night_brightness_.optional_value(x...);
+    if (night_brightness.has_value()) {
+      stair_lighting_->set_night_brightness(*night_brightness);
+    }
+  }
+
+ protected:
+  StairLightingComponent *stair_lighting_;
+};
+
+template<typename... Ts> class StairLightingTurnOffAction : public Action<Ts...> {
+ public:
+  StairLightingTurnOffAction(StairLightingComponent *stair_lighting) : stair_lighting_(stair_lighting) {}
+
+  void play(Ts... x) override {
+    stair_lighting_->set_effect_brightness(0);
+    stair_lighting_->set_night_brightness(0);
+  }
+
+ protected:
+  StairLightingComponent *stair_lighting_;
 };
 
 }  // namespace stair_lighting
