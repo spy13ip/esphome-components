450e21fd12330eb8887a3d193787ba94b49deca2
Valeriy Ievskiy <v.ievskii@bftcom.com>
2024-11-25 20:37:51 +0300

    Добавлено логирование

--- esphome/components/stair_lighting/stair_lighting.cpp
+++ esphome/components/stair_lighting/stair_lighting.cpp
@@ -1,21 +1,26 @@
 #include "stair_lighting.h"
-#include "esphome/components/light/addressable_light_effect.h"
+#include "esphome/core/log.h"
 
 namespace esphome {
 namespace stair_lighting {
+
+static const char *const TAG = "stair_lighting";
 
 void StairLightingComponent::setup() {}
 
 void StairLightingComponent::update() {}
 
 void StairLightingComponent::turn_on() {
+  ESP_LOGD(TAG, "Effect brightness - %f", this->effect_brightness_);
   for (auto *step : steps_) {
+    ESP_LOGD(TAG, "Step processing - %s", step->get_name().c_str());
     step->make_call().set_effect("action").set_brightness(effect_brightness_).perform();
   }
 }
 
 void StairLightingComponent::turn_off() {
   for (auto *step : steps_) {
+
     step->make_call().set_effect("night").set_brightness(night_brightness_).perform();
   }
 }
