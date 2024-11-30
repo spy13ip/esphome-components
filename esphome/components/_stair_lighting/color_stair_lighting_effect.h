1749906c3882e1186cc997127c92327aa3437ad0
Valeriy Ievskiy <v.ievskii@bftcom.com>
2024-11-30 23:25:40 +0300

    Добавление эффекта

---
+++ esphome/components/_stair_lighting/color_stair_lighting_effect.h
@@ -0,0 +1,48 @@
+#pragma once
+
+#include <utility>
+#include <vector>
+#include <algorithm>
+
+#include "stair_lighting_effect.h"
+
+using std::vector;
+using namespace esphome::light;
+
+namespace esphome {
+namespace stair_lighting {
+
+class ColorStairLightingEffect : public StairLightingEffect {
+ public:
+  explicit ColorStairLightingEffect(const std::string &name, const vector<StepConfig> &step_config)
+      : StairLightingEffect(name, step_config) {}
+
+ protected:
+  bool apply(Step &step, const Color &current_color) override {
+    float effect = calculate_brightness(step.effect_data());
+    float night = calculate_brightness(step.night_data());
+    float result = max(effect, night);
+    Color result_color = current_color * (uint8_t) (255.0f * result);
+    for (int i = 0; i < step.size(); i++) {
+      step[i] = result_color;
+    }
+    return true;
+  }
+
+  static float calculate_brightness(const ProgressData &data) {
+    const auto &operation = data.last_operation();
+    const float &increase = data.get(INCREASE);
+    const float &decrease = data.get(DECREASE);
+    if (isnan(increase) && isnan(decrease))
+      return 0;
+    switch (operation) {
+      case INCREASE:
+        return isnan(decrease) ? increase : max(increase, 1.0f - decrease);
+      case DECREASE:
+        return isnan(increase) ? 1.0f - decrease : min(increase, 1.0f - decrease);
+    }
+  }
+};
+
+}  // namespace stair_lighting
+}  // namespace esphome
---
+++ esphome/components/_stair_lighting/effects.py
@@ -0,0 +1,52 @@
+from esphome import automation
+import esphome.codegen as cg
+import esphome.config_validation as cv
+from esphome.const import (
+    CONF_NAME,
+    CONF_UPDATE_INTERVAL,
+)
+
+from esphome.components.light.effects import register_addressable_effect
+from esphome.components.light.types import (
+    AddressableLightEffect,
+)
+
+stair_lighting_ns = cg.esphome_ns.namespace("stair_lighting")
+StepConfig = stair_lighting_ns.struct("StepConfig")
+StairLightingEffect = stair_lighting_ns.class_("StairLightingEffect", AddressableLightEffect)
+ColorStairLightingEffect = stair_lighting_ns.class_("ColorStairLightingEffect", StairLightingEffect)
+
+
+@register_addressable_effect(
+    "color_stair_lighting",
+    ColorStairLightingEffect,
+    "Color Stair Lighting",
+    {
+        cv.Required("steps"): cv.ensure_list(
+            cv.Schema(
+                {
+                    cv.Required("size"): cv.positive_int,
+                    cv.Optional("reversed", default="false"): cv.boolean
+                }
+            )
+        ),
+        cv.Optional(CONF_UPDATE_INTERVAL, default="32ms"): cv.positive_time_period_milliseconds,
+        cv.Optional("next_step_interval", default="350ms"): cv.positive_time_period_milliseconds,
+        cv.Optional("progress_step_interval", default="1s"): cv.positive_time_period_milliseconds,
+    },
+)
+async def addressable_fireworks_effect_to_code(config, effect_id):
+    steps = []
+    for step_config in config.get("steps", []):
+        steps.append(
+            cg.StructInitializer(
+                StepConfig,
+                ("size", step_config["size"]),
+                ("reversed", step_config["reversed"]),
+            )
+        )
+    var = cg.new_Pvariable(effect_id, config[CONF_NAME], steps)
+    cg.add(var.set_update_interval(config[CONF_UPDATE_INTERVAL]))
+    cg.add(var.set_next_step_interval(config["next_step_interval"]))
+    cg.add(var.set_progress_step_interval(config["progress_step_interval"]))
+    return var
