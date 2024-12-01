678095d27cc35097ea784676f48f0be5843a70d0
Valeriy Ievskiy <v.ievskii@bftcom.com>
2024-12-01 19:08:21 +0300

    Переработка

--- esphome/components/stair_lighting/__init__.py
+++ esphome/components/stair_lighting/__init__.py
@@ -14,7 +14,7 @@ stair_lighting_ns = cg.esphome_ns.namespace("stair_lighting")
 
 StairLightingComponent = stair_lighting_ns.class_("StairLightingComponent", cg.Component)
 
-Step = stair_lighting_ns.struct("Step")
+Step = stair_lighting_ns.class_("Step")
 StairLightingEffect = stair_lighting_ns.class_("StairLightingEffect", AddressableLightEffect)
 ColorStairLightingEffect = stair_lighting_ns.class_("ColorStairLightingEffect", StairLightingEffect)
 
@@ -39,7 +39,6 @@ async def to_code(config):
         cv.Required("steps"): cv.ensure_list(
             cv.Schema(
                 {
-                    cv.Optional(CONF_ID): cv.declare_id(Step),
                     cv.Required("size"): cv.positive_int,
                     cv.Optional("reversed", default="false"): cv.boolean
                 }
@@ -57,7 +56,7 @@ async def color_stair_lighting_effect_to_code(config, effect_id):
     cg.add(effect.set_update_interval(config[CONF_UPDATE_INTERVAL]))
     steps = []
     for step_config in config.get("steps", []):
-        step = cg.new_Pvariable(step_config[CONF_ID], step_config["size"], step_config["reversed"])
+        step = cg.new_Pvariable(cv.declare_id(Step), step_config["size"], step_config["reversed"])
         steps.append(step)
     cg.add(effect.add_steps(steps))
     cg.add(effect.set_next_step_interval(config["next_step_interval"]))
