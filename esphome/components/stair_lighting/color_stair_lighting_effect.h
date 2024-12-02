103f96e39dfc52b9e42bbec3a4fc2df234ce3bc3
Valeriy Ievskiy <v.ievskii@bftcom.com>
2024-12-02 22:52:23 +0300

    Переработка

--- esphome/components/stair_lighting/color_stair_lighting_effect.h
+++ esphome/components/stair_lighting/color_stair_lighting_effect.h
@@ -17,21 +17,31 @@ class ColorStairLightingEffect : public StairLightingEffect {
   explicit ColorStairLightingEffect(const std::string &name) : StairLightingEffect(name) {}
 
  protected:
+  Color last_color_ = Color::BLACK;
+
   bool show(const Color &current_color, float brightness, float night_brightness) override {
     bool changed = false;
+    if (last_color_ != current_color) {
+      changed = true;
+    }
+    last_color_ = current_color;
+
     auto first_step = steps_.begin();
     auto last_step = steps_.end() - 1;
     for (auto it = steps_.begin(); it != steps_.end(); it++) {
       auto &step = (**it);
-      float effect_data = step.effect_data().value * brightness;
-      float night_data = (it == first_step || it == last_step) ? night_brightness : 0.0f;
-      float result_data = std::max(effect_data, night_data);
-      Color result_color = current_color * (uint8_t) (255.0f * result_data);
+      float effect_data_f = step.effect_data().value * brightness;
+      if (it == first_step || it == last_step) {
+        effect_data_f = std::max(effect_data_f, night_brightness);
+      }
+      uint8_t effect_data = (uint8_t) (255.0f * effect_data_f);
+      Color result_color = current_color * effect_data;
       for (int i = 0; i < step.size(); i++) {
-        if (step[i].get() != result_color) {
-          step[i].set(result_color);
+        if (effect_data != step[i].get_effect_data()) {
+          step[i].set_effect_data(effect_data);
           changed = true;
         }
+        step[i].set(result_color);
       }
     }
     return changed;
